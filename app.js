5f6dfd5980499d7bcef5f07b2c29a81283707812
jhy950210 <jhyy1995210@gmail.com>
2021-06-15 14:32:30 +0900

    let/const 이해

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-alert("hi")
+const a = 5;
+const b = 2;
+let myName = "wheeyoung";
+
+console.log(a + b);
+console.log(a * b);
+console.log(a / b);
+console.log("hello " + myName);
+
+myName = "jung";
+
+console.log("new name is " + myName);
